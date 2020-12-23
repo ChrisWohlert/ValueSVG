f8f935ad32faf42fb87d8157c3b4db232a29b9f1
ChrisWohlert <chriswohlert@hotmail.com>
2020-12-23 11:05:28 +0100

    halfDial attempt

--- src/ValueSVG.hs
+++ src/ValueSVG.hs
@@ -15,30 +15,59 @@ import           Diagrams.Prelude
 
 newtype Percentage = Percentage { unPercentage :: Double }
 
-data PercentageCircleSettings = PercentageCircleSettings { _percentage :: Percentage, _color :: Colour Double }
+$(makeLenses ''Percentage)
 
-instance Default PercentageCircleSettings where
-    def  = PercentageCircleSettings (Percentage 60) purple
-
-$(makeLenses ''PercentageCircleSettings)
-
-dev = renderSVG "test.svg" (mkWidth 400) $ percentageCircle (Percentage 30) # frame 0.1
+dev = renderSVG "test.svg" (mkWidth 800) $ percentageHalfCircle (Percentage 30) # frame 0.1
 
 percentageCircle :: Percentage -> Diagram B
 percentageCircle (Percentage p) =
     text (show p ++ "%") # scale 0.4
                          # translateY (-0.05)
-    <> dial (Percentage p)
+    <> dial (Percentage p) # fc purple
+                           # lc purple
+                           # opacity 0.5
 
-percentageCircle' settings =
-    ( text ( ++ "%")) # scale 0.4
-                         # translateY (-0.05)
-    <> dial (Percentage p)
-
-dial (Percentage p) = (annularWedge 1 width (rotateBy (1/4) xDir) (360 @@ deg)
-    <> annularWedge 1 width (rotateBy (1/4) xDir) ((-(360 / 100 * p)) @@ deg))
-        # fc purple
-        # lc purple
-        # opacity 0.3
+dial (Percentage p) = annularWedge 1 width (rotateBy (1/4) xDir) (360 @@ deg)
+    <> annularWedge 1 width (rotateBy (1/4) xDir) ((-(360 / 100 * p)) @@ deg)
     where
         width = 0.8
+
+percentageHalfCircle :: Percentage -> Diagram B
+percentageHalfCircle (Percentage p) = 
+    text (show p ++ "%") # scale 0.4
+                         # translateY (-0.05)
+    <> halfDial' (Percentage p) # fc purple
+                               # lc purple
+                               # opacity 0.5
+
+halfDial (Percentage p) = annularWedge 1 width (rotate (315 @@ deg) xDir) (270 @@ deg)
+    <> annularWedge 1 width (rotate (225 @@ deg) xDir) ((-(270 / 100 * p)) @@ deg)
+    <> wedge 0.1 (rotate (225 @@ deg) xDir) (180 @@ deg)
+        # rotate (224.5 @@deg)
+        # moveTo (p2 (0, 0.9))
+        # rotateAround (p2 (0, 0)) (135.5 @@ deg)
+    <> wedge 0.1 (rotate (225 @@ deg) xDir) (180 @@ deg)
+        # rotate (45.5 @@deg)
+        # moveTo (p2 (0, 0.9))
+        # rotateAround (p2 (0, 0)) (224.5 @@ deg)
+        # opacity 1.5
+    where
+        width = 0.8
+
+halfDial' (Percentage p) = annularWedge 1 width (rotate (315 @@ deg) xDir) (270 @@ deg)
+    <> annularWedge 1 width (rotate (225 @@ deg) xDir) ((-(270 / 100 * p)) @@ deg)
+    <> wedge 0.1 (rotate (225 @@ deg) xDir) (180 @@ deg)
+        # rotate (224.5 @@deg)
+        # moveTo (p2 (0, 0.9))
+        # rotateAround (p2 (0, 0)) (135.5 @@ deg)
+    <> wedge 0.1 (rotate (225 @@ deg) xDir) (180 @@ deg)
+        # rotate (45.5 @@deg)
+        # moveTo (p2 (0, 0.9))
+        # rotateAround (p2 (0, 0)) (224.5 @@ deg)
+        # opacity 1.5
+    where
+        width = 0.8
+
+
+
+-- wedge 0.1 (rotate (225 @@ deg) xDir) (180 @@ deg) # translate (V2 (-0.631) (-0.642))
